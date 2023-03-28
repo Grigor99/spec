461b8e3ff9d60d7450b43fa1bdeb49f8e33c2104
Grigor99 <80825619+Grigor99@users.noreply.github.com>
2023-03-28 15:58:57 +0400

    Delete src/test directory

--- src/test/java/com/example/specification/concurrent/ConcurrentExecution.java
+++
@@ -1,8 +0,0 @@
-package com.example.specification.concurrent;
-
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
-
-@Execution(ExecutionMode.CONCURRENT)
-public interface ConcurrentExecution {
-}
